a2933decdf1192b32085fe805087a220d27deff0
leonardo <leodanlors1993@gmail.com>
2022-01-21 15:29:16 -0600

    card and search

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,58 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="search-bar">
+      <img src="@/assets/search-icon.png" class="search-bar-image">
+      <input type="text"
+             :placeholder="'Buscar categoria'"
+             id="search"
+             v-model="searchParam"
+             class="search-bar-input">
     </div>
-    <router-view />
+    <div class="card-container">
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+    </div>
   </div>
 </template>
-
+<script>
+import Card from "@/components/Card.vue";
+export default {
+  components: { Card },
+};
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&family=Roboto:wght@400;500;700&display=swap');
+$black:#2D3958;
+$background:#FAFBFF;
+$white:#ffff;
+$purple:#6A35FF;
+body{
+   background-color:$background;
+}
+p{
+  margin:0;
+}
+.search-bar{
+  display: grid;
+  grid-auto-flow: column;
+  grid-template-columns: auto 1fr;
+  grid-column-gap: 5px;
+  border: solid 1px $purple;
+  border-radius: 10px;
+  height:31px;
+  padding:3px 6px;
+}
+.search-bar-image{
+  width: 20px;
+  align-self: center;
+  justify-self: center;
+}
+.search-bar-input{
+  border:none;
+  outline: none;
+  border-radius: 10px;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="card">
+    <img
+      src="https://resources.plerk.io/category/music.png"
+      alt="category Image"
+      class="card-image"
+    />
+    <div class="card-content">
+        <p class="card-category">Música</p>
+    <p class="card-price">$10 USD</p>
+    <p class="card-category-type">Tipo de categoria: Normal</p>
+    </div>
+    
+  </div>
+</template>
+<script>
+export default {
+  name: "Card",
+  props: {
+    msg: String,
+  },
+};
+</script>
+<style lang="scss">
+$black:#2D3958;
+$background:#FAFBFF;
+$white:#ffff;
+$purple:#6A35FF;
+    .card{
+        background:$white;
+        border-radius:10px;
+        width: 341px;
+        height: 188px;
+        filter: drop-shadow(1px 2px 5px rgba(123, 136, 168, 0.08));
+        display: grid;
+        grid-auto-flow: column;
+        grid-column-gap: 10px;
+        padding:20px 10px;
+        box-sizing: border-box;
+        margin:auto;
+    }
+    .card-image{
+        width:150px;
+        justify-self: center;
+        align-self: center;
+    }
+    .card-content{
+        display:grid;
+        grid-row-gap: 15px;
+        grid-template-rows: auto auto 1fr;
+    }
+    .card-category{
+        font-size:24px;
+        font-family: Montserrat;
+        font-weight: 600;
+    }
+    .card-price{
+        font-size: 18px;
+        font-family:Roboto;
+    }
+    .card-category-type{
+        font-size:12px;
+        font-family: Roboto;
+        align-self: end;
+    }
+</style>
