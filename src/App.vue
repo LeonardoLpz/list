3f7a15976828771ee91a9ba3e73918803d52fffb
leonardo <leodanlors1993@gmail.com>
2022-01-21 19:53:55 -0600

    responsive

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app">
     <div class="search-bar">
-      <img src="@/assets/search-icon.png" class="search-bar-image">
-      <input type="text"
-             :placeholder="'Buscar categoria'"
-             id="search"
-             v-model="searchParam"
-             class="search-bar-input">
+      <img src="@/assets/search-icon.png" class="search-bar-image" />
+      <input
+        type="text"
+        :placeholder="'Buscar categoria'"
+        id="search"
+        v-model="searchParam"
+        class="search-bar-input"
+      />
     </div>
+
     <div class="card-container">
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
       <Card></Card>
       <Card></Card>
       <Card></Card>
@@ -20,39 +26,51 @@
 import Card from "@/components/Card.vue";
 export default {
   components: { Card },
+  methods: {
+    
+  }
 };
 </script>
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&family=Roboto:wght@400;500;700&display=swap');
-$black:#2D3958;
-$background:#FAFBFF;
-$white:#ffff;
-$purple:#6A35FF;
-body{
-   background-color:$background;
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&family=Roboto:wght@400;500;700&display=swap");
+$black: #2d3958;
+$background: #fafbff;
+$white: #ffff;
+$purple: #6a35ff;
+body {
+  background-color: $background;
 }
-p{
-  margin:0;
+p {
+  margin: 0;
 }
-.search-bar{
+.search-bar {
   display: grid;
   grid-auto-flow: column;
   grid-template-columns: auto 1fr;
   grid-column-gap: 5px;
   border: solid 1px $purple;
   border-radius: 10px;
-  height:31px;
-  padding:3px 6px;
+  height: 31px;
+  padding: 3px 6px;
+  max-width: 425px;
+  margin-left: auto;
 }
-.search-bar-image{
+.search-bar-image {
   width: 20px;
   align-self: center;
   justify-self: center;
 }
-.search-bar-input{
-  border:none;
+.search-bar-input {
+  border: none;
   outline: none;
   border-radius: 10px;
 }
-
+.card-container {
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 1280px;
+  margin: auto;
+  justify-content: center;
+  margin-top: 40px;
+}
 </style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -6,11 +6,10 @@
       class="card-image"
     />
     <div class="card-content">
-        <p class="card-category">Música</p>
-    <p class="card-price">$10 USD</p>
-    <p class="card-category-type">Tipo de categoria: Normal</p>
+      <p class="card-category">Música</p>
+      <p class="card-price">$10 USD</p>
+      <p class="card-category-type">Tipo de categoria: Normal</p>
     </div>
-    
   </div>
 </template>
 <script>
@@ -22,45 +21,46 @@ export default {
 };
 </script>
 <style lang="scss">
-$black:#2D3958;
-$background:#FAFBFF;
-$white:#ffff;
-$purple:#6A35FF;
-    .card{
-        background:$white;
-        border-radius:10px;
-        width: 341px;
-        height: 188px;
-        filter: drop-shadow(1px 2px 5px rgba(123, 136, 168, 0.08));
-        display: grid;
-        grid-auto-flow: column;
-        grid-column-gap: 10px;
-        padding:20px 10px;
-        box-sizing: border-box;
-        margin:auto;
-    }
-    .card-image{
-        width:150px;
-        justify-self: center;
-        align-self: center;
-    }
-    .card-content{
-        display:grid;
-        grid-row-gap: 15px;
-        grid-template-rows: auto auto 1fr;
-    }
-    .card-category{
-        font-size:24px;
-        font-family: Montserrat;
-        font-weight: 600;
-    }
-    .card-price{
-        font-size: 18px;
-        font-family:Roboto;
-    }
-    .card-category-type{
-        font-size:12px;
-        font-family: Roboto;
-        align-self: end;
-    }
+$black: #2d3958;
+$background: #fafbff;
+$white: #ffff;
+$purple: #6a35ff;
+.card {
+  background: $white;
+  border-radius: 10px;
+  width: 341px;
+  height: 188px;
+  filter: drop-shadow(1px 2px 5px rgba(123, 136, 168, 0.08));
+  display: grid;
+  grid-auto-flow: column;
+  grid-column-gap: 10px;
+  padding: 20px 10px;
+  box-sizing: border-box;
+  margin-inline-end: 20px;
+  margin-block-end: 20px;
+}
+.card-image {
+  width: 150px;
+  justify-self: center;
+  align-self: center;
+}
+.card-content {
+  display: grid;
+  grid-row-gap: 15px;
+  grid-template-rows: auto auto 1fr;
+}
+.card-category {
+  font-size: 24px;
+  font-family: Montserrat;
+  font-weight: 600;
+}
+.card-price {
+  font-size: 18px;
+  font-family: Roboto;
+}
+.card-category-type {
+  font-size: 12px;
+  font-family: Roboto;
+  align-self: end;
+}
 </style>
